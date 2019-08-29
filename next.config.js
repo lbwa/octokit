4bccde2af6b0b6626c7c2271a7be576d988f30cd
lbwa <mr_lbw@outlook.com>
2019-08-30 01:19:30 +0800

    chore: add deployment script

--- next.config.js
+++ next.config.js
@@ -6,5 +6,17 @@ module.exports = {
       use: 'raw-loader'
     })
     return config
+  },
+  /**
+   * @static_路由 实际访问的路由
+   * @pages_路由 对应 pages 文件夹的层级结构
+   * @description 用于定义 static 路由与 pages 路由的映射
+   * @doc https://nextjs.org/docs#static-html-export
+   * @param {*} defaultPathMap
+   */
+  exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      '/': { page: '/' }
+    }
   }
 }
