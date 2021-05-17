4019296ab976bd93a59a2275720b0cd22ee1ffa0
lbwa <lbwa@users.noreply.github.com>
2021-05-17 01:28:29 +0000

    deploy: from Github action - 01:28:29

---
+++ _next/static/iI4d8UUy-EEGnsJvPQ7U_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-3316a3ee9462ec51ddc3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6022074b5d1eb6a3b9bc.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
